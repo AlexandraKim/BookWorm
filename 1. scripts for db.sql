b98eded56b39cb4012ddcf238aa533f455760b1a
Alexandra Kim <Alexandra.success.kim@gmail.com>
2019-12-13 13:42:44 +0500

    script fixed

--- 1. scripts for db.sql
+++ 1. scripts for db.sql
@@ -16,7 +16,7 @@ Type int(6) not null,
 FirstName varchar(128) not null,
 LastName varchar(128) not null,
 Password varchar(32) not null,
-IsBlocked bit,
+IsBlocked bit
 );
 
 CREATE TABLE UserToBook (
@@ -46,9 +46,9 @@ insert into book (Title, Genre, Author, Isbn, PublicationDate) values ('Pride an
 insert into book (Title, Genre, Author, Isbn, PublicationDate) values ('Twilight', ' Fantasy Fiction', 'Stephenie Meyer', '5489632015', '2005-10-05');
 insert into book (Title, Genre, Author, Isbn, PublicationDate) values ('The Fault in Our Stars', 'Science  Fiction', 'John Green', '1239620852', '2012-01-10');
 insert into book (Title, Genre, Author, Isbn, PublicationDate) values ('The Da Vinci Code', 'Detective', 'Dan Brown', '5632033881', '2003-03-18');
-insert into book (Title, Genre, Author, Isbn, PublicationDate) values ('Alice's Adventures in Wonderland & Through the Looking-Glass', 'Fantasy', 'Lewis Carrol', '9875220015', '2000-12-01');
+insert into book (Title, Genre, Author, Isbn, PublicationDate) values ('Alice''s Adventures in Wonderland & Through the Looking-Glass', 'Fantasy', 'Lewis Carrol', '9875220015', '2000-12-01');
 insert into book (Title, Genre, Author, Isbn, PublicationDate) values ('Jane Eyre', 'Novel Romantic', 'Chalotte Bronte', '8755236697', '1847-10-16');
-insert into book (Title, Genre, Author, Isbn, PublicationDate) values ('The Little Prince', 'Fabel Novel', 'Antoine de Saint-Exupéry', '5223367890', '1943-04-06');
+insert into book (Title, Genre, Author, Isbn, PublicationDate) values ('The Little Prince', 'Fabel Novel', 'Antoine de Saint-Exupery', '5223367890', '1943-04-06');
 insert into book (Title, Genre, Author, Isbn, PublicationDate) values ('Life Pi', 'Adventure Fantasy', 'Yann Martel', '1029654789', '2001-10-11');
 
 insert into UserToBook (UserId, BookId) values ('U1510001', 1);
---
+++ 2. scripts for db - extend user table.sql
@@ -0,0 +1,3 @@
+alter table user add column phone varchar(15)
+alter table user add column email varchar(128)
+alter table user add column enrollmentYear
