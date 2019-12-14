60d43a9b2f10ce210f201362d46d74d926a29c0f
ZilolaTalipova <talipova.zilola98@gmail.com>
2019-12-14 18:16:48 +0500

    undated window

--- src/Librarian/LibrarianController.java
+++ src/Librarian/LibrarianController.java
@@ -1,4 +1,4 @@
-package Administration;
+package Librarian;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -17,7 +17,7 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class AdminController implements Initializable {
+public class LibrarianController implements Initializable {
 
     @FXML
     private Label label;
@@ -34,13 +34,7 @@ public class AdminController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         //Added new font which is named Montserrat
-        Font myFont ;
 
-        try {
-            myFont = Font.loadFont(new FileInputStream(new File("Montserrat")), 10);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
         //List of information,  here should be add functionaly
         Node[] nodes = new Node[15];
         for (int i = 0; i < nodes.length ; i++){
--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,16 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.awt.*;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("Administration/Admin.fxml"));
         primaryStage.setTitle("Log In BookWorm");
-        primaryStage.setScene(new Scene(root, 950, 630));
+        primaryStage.setScene(new Scene(root, 1366, 768));
+
         primaryStage.show();
     }
     public static void main(String[] args) {
