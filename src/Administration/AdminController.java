b6f2f72508d107226673d864ac70ba84d9c62c72
Alexandra Kim <Alexandra.success.kim@gmail.com>
2019-12-17 15:54:13 +0500

    name

--- src/Administration/AdminController.java
+++ src/Administration/AdminController.java
@@ -1,15 +1,16 @@
 package Administration;
 
+import Persistence.UserEntity;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
+import Utils.*;
 
-import javax.swing.*;
-import java.awt.*;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -29,10 +30,16 @@ public class AdminController implements Initializable {
     @FXML
     private Label numS1;
 
+    @FXML
+    public Label userName;
 
+    private UserEntity _user = Auth.getUser();
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        userName.setText(_user.getFirstName() + " " + _user.getLastName());
+
+        
         //Added new font which is named Montserrat
         Font myFont ;
 
